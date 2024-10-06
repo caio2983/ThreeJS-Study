4a448539609d78b54bb5eff316fc7a1c05d5e610
caio2983 <caioluna2002@gmail.com>
2024-10-06 18:53:55 -0300

    feat: added orbit controls

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
+
+controls.update();
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 'blue' } );
@@ -19,6 +28,8 @@ function animate() {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+
+    controls.update();
 
 	renderer.render( scene, camera );
 
