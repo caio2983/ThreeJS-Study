27dda3a2031c6bd83838addadab13b77c9d81175
caio2983 <caioluna2002@gmail.com>
2024-10-06 19:14:22 -0300

    feat: the cube is now movable with WASD keys

--- main.js
+++ main.js
@@ -3,19 +3,16 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-
-
 const renderer = new THREE.WebGLRenderer();
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const controls = new OrbitControls( camera, renderer.domElement );
-
-
-
-controls.update();
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 'blue' } );
@@ -24,13 +21,39 @@ scene.add( cube );
 
 camera.position.z = 5;
 
+
+
+
 function animate() {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
     controls.update();
-
+  
 	renderer.render( scene, camera );
 
+
+}
+
+document.onkeydown = function(e) {
+    if(e.keyCode === 65) {
+        cube.position.x -= 1;
+        console.log("teste")
+    }
+
+    if(e.keyCode === 68) {
+        cube.position.x += 1;
+        console.log("teste")
+    }
+
+    if(e.keyCode === 83) {
+        cube.position.y -= 1;
+        console.log("teste")
+    }
+
+    if(e.keyCode === 87) {
+        cube.position.y += 1;
+        console.log("teste")
+    }
 }
