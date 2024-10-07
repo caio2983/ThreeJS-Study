09ccb3e428b6ca311d428b58196523527dca24a2
caio2983 <caioluna2002@gmail.com>
2024-10-06 21:19:49 -0300

    fix: the cubes now move when user changes between routes

--- main.js
+++ main.js
@@ -41,27 +41,24 @@ function animate() {
 
 }
 
-document.onkeydown = function(e) {
-    if(e.keyCode === 65) {
+window.addEventListener("keydown", function(e) {
+    if (e.keyCode === 65) { // A
         cube.position.x -= 1;
-   
     }
-
-    if(e.keyCode === 68) {
+    
+    if (e.keyCode === 68) { // D
         cube.position.x += 1;
-
     }
 
-    if(e.keyCode === 83) {
+    if (e.keyCode === 83) { // S
         cube.position.y -= 1;
-
     }
 
-    if(e.keyCode === 87) {
+    if (e.keyCode === 87) { // W
         cube.position.y += 1;
- 
     }
-}
+});
+
 
 }
 
@@ -84,8 +81,8 @@ function initThreeJsPage2(containerId) {
     
     const geometry = new THREE.BoxGeometry( 1, 1, 1 );
     const material = new THREE.MeshBasicMaterial( { color: 'red' } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    const cube2 = new THREE.Mesh( geometry, material );
+    scene.add( cube2 );
     
     camera.position.z = 5;
     
@@ -94,8 +91,8 @@ function initThreeJsPage2(containerId) {
     
     function animate() {
     
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        cube2.rotation.x += 0.01;
+        cube2.rotation.y += 0.01;
     
         controls.update();
       
@@ -104,56 +101,40 @@ function initThreeJsPage2(containerId) {
     
     }
     
-    document.onkeydown = function(e) {
-        if(e.keyCode === 65) {
-            cube.position.x -= 1;
-       
+    window.addEventListener("keydown", function(e) {
+        if (e.keyCode === 65) { // A
+            cube2.position.x -= 1;
+        }
+        
+        if (e.keyCode === 68) { // D
+            cube2.position.x += 1;
         }
     
-        if(e.keyCode === 68) {
-            cube.position.x += 1;
-    
+        if (e.keyCode === 83) { // S
+            cube2.position.y -= 1;
         }
     
-        if(e.keyCode === 83) {
-            cube.position.y -= 1;
-    
+        if (e.keyCode === 87) { // W
+            cube2.position.y += 1;
         }
-    
-        if(e.keyCode === 87) {
-            cube.position.y += 1;
-     
-        }
-    }
-    
-    }
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const anchors = document.querySelectorAll("a"); 
-
-    anchors.forEach(anchor => {
-        anchor.addEventListener("click", (event) => {
-           
-            const href = anchor.getAttribute("href"); 
-            page(href);
-            
-        });
     });
-});
+    
 
+    }
 
 
 
 function showPage(pageId) {
     document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
     document.getElementById(pageId).classList.add('active');
+ 
 }
 
 page('/', () => {
     showPage('home');
     // resetScene();
     initThreeJs('home'); 
+
     
 });
 
